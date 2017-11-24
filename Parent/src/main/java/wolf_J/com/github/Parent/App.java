90961042fe7a18644e64f398ae1c442772464e90
wolf-J <1510156610@qq.com>
2017-11-25 01:24:49 +0800

    create github project

---
+++ Parent/src/main/java/wolf_J/com/github/Parent/App.java
@@ -0,0 +1,13 @@
+package wolf_J.com.github.Parent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
